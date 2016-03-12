b08978c43980a6d4de3128d2e4e2d8cec186f69a
Veydh <vgooljar@gmail.com>
2016-03-12 17:07:18 -0400

    Got current balance ('Available") which is a better track as to when a withdrawal is done with linx.'

--- FCBManager.py
+++ FCBManager.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 from config import *
 from selenium import webdriver
-#from selenium.webdriver.support.select import Select
 import os
 
 phantomjs = os.path.realpath(os.path.join(os.getcwd(), "phantomjs"))
@@ -16,39 +15,6 @@ password.send_keys(account_password)
 
 form = browser.find_element_by_id('noautocomplete')
 form.submit()
-#------------------------------------------------------------------------
-'''
-maidenname = browser.find_element_by_name('mothersMaidenName')
-maidenname.send_keys(account_maidenname)
-
-email = browser.find_element_by_name('email')
-email.send_keys(account_email)
-
-phonenumber = browser.find_element_by_name('mobilePhoneNumber')
-phonenumber.send_keys(account_phonenumber)
-
-pin = browser.find_element_by_name('transactionPin')
-pin.send_keys(account_pin)
-
-tpin = browser.find_element_by_name('transactionPinVerify')
-tpin.send_keys(account_pin)
-
-
-hint = browser.find_element_by_name('hint')
-hint.send_keys(account_hint)
-
-secretQuest = Select(browser.find_element_by_id("secretQuestion"))
-secretQuest.select_by_value(account_secretquestion)
-
-secretAns = browser.find_element_by_name('secretAnswer')
-secretAns.send_keys(account_secretanswer)
-
-form = browser.find_element_by_id('noautocomplete')
-form.submit()
-
-print browser.page_source
-'''
- #------------------------------------------------------------------------
 
 url = r"https://www.firstcitizenstt.net/accountList.do"
 browser.get(url)
@@ -68,3 +34,14 @@ total_balance = zip(total_balance_type, total_balance_amount)
 print total_balance
 
 #GET CURRENT BALANCE
+total_available = []
+total_available_type = []
+total_available_amount = []
+alltr =  soup.find_all("tr", {"class":["odd", "even"]},recursive=True)
+for tr in alltr:
+    #print tr.contents[1].get_text() + " " + tr.contents[-1].get_text()
+    total_available_type.append(tr.contents[1].get_text())
+    total_available_amount.append(tr.contents[-1].get_text())
+total_available = zip(total_available_type, total_available_amount)
+print total_available
+
