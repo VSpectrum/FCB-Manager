017e20572a735f748e953509d7dc88de7f95d7f8
Veydh <vgooljar@gmail.com>
2016-03-22 23:07:43 -0400

    Scheduler + Emailer integrated such that every hour, an email is sent given that a change occurred in the accounts (observable via accountlog.txt)

--- FCBManager.py
+++ FCBManager.py
@@ -1,50 +1,81 @@
 from bs4 import BeautifulSoup
-from config import *
 from selenium import webdriver
-import os
+import os, schedule, time
 
-phantomjs_executable = ''
-if os.name=='nt': phantomjs_executable = "phantomjs.exe"
-else: phantomjs_executable = "phantomjs"
-phantomjs = os.path.realpath(os.path.join(os.getcwd(), phantomjs_executable))
-browser = webdriver.PhantomJS(phantomjs)
+from config import *
+from emailer import *
 
-url = r"https://www.firstcitizenstt.net/"
-browser.get(url)
+def checkbank():
+    phantomjs_executable = ''
+    if os.name=='nt': phantomjs_executable = "phantomjs.exe"
+    else: phantomjs_executable = "phantomjs"
+    phantomjs = os.path.realpath(os.path.join(os.getcwd(), phantomjs_executable))
+    browser = webdriver.PhantomJS(phantomjs)
+    url = r"https://www.firstcitizenstt.net/"
 
-username = browser.find_element_by_name('userName')
-username.send_keys(account_number)
-password = browser.find_element_by_name('password')
-password.send_keys(account_password)
+    browser.get(url)
 
-form = browser.find_element_by_id('noautocomplete')
-form.submit()
+    username = browser.find_element_by_name('userName')
+    username.send_keys(account_number)
+    password = browser.find_element_by_name('password')
+    password.send_keys(account_password)
 
-url = r"https://www.firstcitizenstt.net/accountList.do"
-browser.get(url)
-response = browser.page_source
-soup = BeautifulSoup(response, "html.parser")
+    form = browser.find_element_by_id('noautocomplete')
+    form.submit()
 
-#TOTAL BALANCE
-total_balance = []
-total_balance_type = []
-total_balance_amount = []
-for td in soup.find_all('div', {'class': 'column'}):
-    for span in td.find_all('dt'):
-        total_balance_type.append(span.text)
-    for span in td.find_all('dd'):
-        total_balance_amount.append(span.text)
-total_balance = zip(total_balance_type, total_balance_amount)
-print total_balance
+    url = r"https://www.firstcitizenstt.net/accountList.do"
+    browser.get(url)
+    response = browser.page_source
+    soup = BeautifulSoup(response, "html.parser")
 
-#GET CURRENT BALANCE
-total_available = []
-total_available_type = []
-total_available_amount = []
-alltr =  soup.find_all("tr", {"class":["odd", "even"]},recursive=True)
-for tr in alltr:
-    total_available_type.append(tr.contents[1].get_text())
-    total_available_amount.append(tr.contents[-1].get_text())
-total_available = zip(total_available_type, total_available_amount)
-print total_available
+    #TOTAL BALANCE
+    total_balance = []
+    total_balance_type = []
+    total_balance_amount = []
+    for td in soup.find_all('div', {'class': 'column'}):
+        for span in td.find_all('dt'):
+            total_balance_type.append(span.text)
+        for span in td.find_all('dd'):
+            total_balance_amount.append(span.text)
+    total_balance = zip(total_balance_type, total_balance_amount)
 
+    browser.quit();
+
+    #GET CURRENT BALANCE
+    total_available = []
+    total_available_type = []
+    total_available_amount = []
+    alltr =  soup.find_all("tr", {"class":["odd", "even"]},recursive=True)
+    for tr in alltr:
+        total_available_type.append(tr.contents[1].get_text())
+        total_available_amount.append(tr.contents[-1].get_text())
+    total_available = zip(total_available_type, total_available_amount)
+
+    email_subject = ""
+    email_body = ""
+    '''
+    for amount in total_balance:
+        email_body+= (amount[0]+": "+amount[1]+"\n")
+    email_body+="\n"
+    '''
+    for amount in total_available:
+        email_body+= (amount[0]+": "+amount[1]+"\n")
+        email_subject += (amount[0]+": "+amount[1]+" | ")
+
+    email_subject = email_subject[:-2] + " - FCB"
+    email_subject = "FCB Account Update"
+
+    account_body = ""
+    with open('accountlog.txt', 'r') as accountlog:
+        account_body = accountlog.read()
+
+    if account_body != email_body:
+        with open('accountlog.txt', 'w') as accountlog:
+            accountlog.write(email_body)
+        send_email("vgooljar@gmail.com", email_subject, email_body)
+
+schedule.every().hour.do(checkbank)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
---
+++ emailer.py
@@ -0,0 +1,27 @@
+from config import *
+
+def send_email(recipient, subject, body):
+    import smtplib
+
+    gmail_user = emailer_ad
+    gmail_pwd = emailer_pass
+    FROM = emailer_ad
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'Successfully sent mail'
+    except:
+        print "Failed to send mail"
+
+#send_email("vgooljar@gmail.com", "Test Subject", "Test Body")
